e7e1411c78b269451da62b8928d4bd0672fbdc27
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2023-10-11 14:36:51 +0100

    Create 3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Old school band
+-- SELECT band_nme
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
